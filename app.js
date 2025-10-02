c99be03bbcfc70064acae97e666573d8aa7b73b9
Techflux <shemtmwangi@gmail.com>
2025-10-02 15:30:03 +0300

    JavaScript manenos

---
+++ app.js
@@ -0,0 +1,25 @@
+const cityInput = document.querySelector('.city-input')
+const searchBtn= document.querySelector('.search-btn')
+
+searchBtn.addEventListener('click', () => {
+    if (cityInput.value.trim() != '') {
+        updateWeatherInfo(cityInput.value)
+        cityInput.value = ''
+        cityInput.blur()
+    }
+})
+cityInput.addEventListener('keydown', (event) => {
+    if (event.key == 'Enter' && cityInput.value.trim() != '') {
+        updateWeatherInfo(cityInput.value)
+        cityInput.value = ''
+        cityInput.blur()
+    }
+})
+
+function getFetchData() {
+    //Video=28:10
+}
+
+function updateWeatherInfo(city) {
+    const weatherDate = getFetchData()
+}
